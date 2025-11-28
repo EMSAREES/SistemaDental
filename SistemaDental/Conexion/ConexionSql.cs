e55aed5e3b855892ead356cbf45524ab6f37bff8
EMSAREES <lobomategamer@gmail.com>
2025-11-27 19:14:10 -0700

    first commit

---
+++ SistemaDental/Conexion/ConexionSql.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDental.Conexion
+{
+    internal class ConexionSql
+    {
+        public static string conectar()
+        {
+            string con = @"DATA Source=EMILIO\SQLEXPRESS;INITIAL CATALOG=Dental_Db;USER=sa;PASSWORD=tucontraseña";
+
+            return con;
+        }
+    }
+}
